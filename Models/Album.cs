05d4ba7e31507d2150f8a121576e2e908007d981
Rodrigo Leite <rodrigo@leite.dev>
2019-09-02 18:43:02 -0300

    Fix model types

--- Models/Album.cs
+++ Models/Album.cs
@@ -2,12 +2,12 @@ using System.Collections.Generic;
 
 namespace Station.Models {
     public class Album {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
         public string CoverArt { get; set; }
         public string MBID { get; set; }
         public string Type { get; set; }
-        public List<string> Tags { get; set; }
+        public string[] Tags { get; set; }
         public List<Song> Songs { get; set; }
     }
 }
